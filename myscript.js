099d9e2028b0d64fd4b5cc34842b82348122d766
Barry Scott <burry.scott*@gmail.com>
2024-02-20 21:07:27 -0500

    add reset button

--- myscript.js
+++ myscript.js
@@ -1,21 +1,35 @@
-div = document.getElementById('container');
+let div = document.getElementById('container');
+let userInput;
+let userInputSquared;
 
-let userInput = prompt('Pick a number');
+function getUserInput() {
+    do {    
+        userInput = prompt('Pick a number 1-100');
+    } while (userInput.isNaN || userInput > 100 || userInput < 0); 
+    userInputSquared = userInput * userInput;
+    return userInputSquared
+}
 
-let userInputSquared = userInput * userInput;
-let squareSize = 100 / userInput;
-
-console.log(squareSize);
-for (let i = 0; i < userInputSquared; i++) {
+function createGrid(userInputSquared) { for (let i = 0; i < userInputSquared; i++) {
     let square = document.createElement('div');
     div.appendChild(square);
+    let squareSize = 100 / userInput;
     square.style.flex = '1 1 ' + squareSize + '%';
     square.className = 'squareClass';
     square.style.backgroundColor = 'grey';
-}
+}}
 
-const squares = document.querySelectorAll('div.squareClass');
-
+function changeSquares() {
+let squares = document.querySelectorAll('div.squareClass');
 squares.forEach(e => e.addEventListener('mouseover', (event) => {
     event.target.style.backgroundColor = 'blue';
-}));
+}));}
+
+document.addEventListener('click', (event) => {
+    if (event.target.className == 'button') {
+        getUserInput();
+        createGrid(userInputSquared);
+        changeSquares();
+        return;
+    }
+})
